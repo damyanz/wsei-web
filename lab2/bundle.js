8a9b06dc7e56b58feabdca3bca84adb29d3e0f95
damyanz <damyan.z@o2.pl>
2021-04-05 20:56:38 +0200

    Add required features TODO: add types

--- lab2/bundle.js
+++ lab2/bundle.js
@@ -45,15 +45,148 @@ var SOUNDS = [
         key: "o"
     },
 ];
+var Track = /** @class */ (function () {
+    function Track(wrapper) {
+        this.memory = [];
+        this.isRecording = false;
+        this.isPlaying = false;
+        this.isMuted = false;
+        this.currentProgress = 0;
+        this.create(wrapper);
+    }
+    Track.prototype.createTrackControls = function () {
+        var _this = this;
+        var controlsWrapper = document.createElement("div");
+        controlsWrapper.classList.add("controlsWrapper");
+        var muteIcon = document.createElement("span");
+        muteIcon.classList.add("muteIcon");
+        muteIcon.addEventListener("click", function () {
+            if (!_this.isMuted) {
+                muteIcon.classList.add("muteIcon--muted");
+                _this.isMuted = true;
+            }
+            else {
+                muteIcon.classList.remove("muteIcon--muted");
+                _this.isMuted = false;
+            }
+        });
+        var playButton = document.createElement("button");
+        playButton.classList.add("button", "playButton");
+        playButton.innerText = "ODTWORZ";
+        playButton.addEventListener("click", function (e) {
+            if (_this.memory.length > 0) {
+                if (!_this.isPlaying) {
+                    _this.playRecording();
+                }
+            }
+        });
+        var recordButton = document.createElement("button");
+        recordButton.classList.add("button", "recordButton");
+        recordButton.innerText = "NAGRYWAJ";
+        recordButton.addEventListener("click", function () {
+            if (!_this.isRecording) {
+                recordButton.classList.add("recordButton--recording");
+                _this.startRecording();
+            }
+            else {
+                recordButton.classList.remove("recordButton--recording");
+                _this.stopRecording();
+            }
+        });
+        controlsWrapper.appendChild(muteIcon);
+        controlsWrapper.appendChild(recordButton);
+        controlsWrapper.appendChild(playButton);
+        return controlsWrapper;
+    };
+    Track.prototype.create = function (wrapper) {
+        var trackWrapper = document.createElement("div");
+        trackWrapper.classList.add("trackWrapper");
+        var trackControls = this.createTrackControls();
+        var el = document.createElement("div");
+        el.classList.add("track");
+        trackWrapper.appendChild(trackControls);
+        trackWrapper.appendChild(el);
+        wrapper.appendChild(trackWrapper);
+        this.trackElement = el;
+    };
+    Track.prototype.clearMemory = function () {
+        this.memory = [];
+        this.duration = 0;
+        this.recordingStartTimestamp = new Date().getTime();
+    };
+    Track.prototype.recordSound = function (audioElement) {
+        var recordTimestamp = new Date().getTime();
+        var soundData = {
+            audioElement: audioElement,
+            recordTime: recordTimestamp - this.recordingStartTimestamp
+        };
+        this.memory.push(soundData);
+    };
+    Track.prototype.startRecording = function () {
+        this.clearMemory();
+        this.isRecording = true;
+    };
+    Track.prototype.stopRecording = function () {
+        var recordingEndTimestamp = new Date().getTime();
+        this.duration = recordingEndTimestamp - this.recordingStartTimestamp;
+        console.log(this.duration);
+        this.isRecording = false;
+    };
+    Track.prototype.playRecording = function () {
+        this.isPlaying = true;
+        this.runTrackProgress();
+        this.memory.forEach(function (soundData, soundIndex, soundsArr) {
+            setTimeout(function () {
+                playSound(soundData.audioElement);
+            }, soundData.recordTime);
+        });
+    };
+    Track.prototype.runTrackProgress = function () {
+        var _this = this;
+        var interval = 1000 / 60;
+        var progressInterval = setInterval(function () {
+            _this.currentProgress =
+                _this.currentProgress + (interval / _this.duration) * 100;
+            if (_this.currentProgress >= 100) {
+                _this.currentProgress = 100;
+                _this.isPlaying = false;
+                _this.stopTrackProgress();
+                clearInterval(progressInterval);
+            }
+            _this.trackElement.style.background = "linear-gradient(to right, red,  red " + _this.currentProgress + "%, gray " + _this.currentProgress + "%, gray 100%)";
+        }, interval);
+    };
+    Track.prototype.stopTrackProgress = function () {
+        var _this = this;
+        setTimeout(function () {
+            _this.trackElement.style.background = "";
+            _this.currentProgress = 0;
+        }, 500);
+    };
+    Track.prototype.removeRecording = function () {
+        this.memory = [];
+    };
+    return Track;
+}());
+var tracks = [];
 function initApp() {
     createAudioElements();
     createControls();
+    createTracks();
     addKeyListener();
+    handlePlayAllButton();
 }
 var AudioControls = {
     elements: {},
     controls: {}
 };
+function createTracks() {
+    for (var i = 0; i < 4; i++) {
+        var wrapper = document.querySelector("#tracksWrapper");
+        var track = new Track(wrapper);
+        tracks.push(track);
+    }
+}
 function createAudioElements() {
     var wrapper = document.querySelector("#audioWrapper");
     SOUNDS.forEach(function (sound) {
@@ -84,6 +217,14 @@ function handleKeyPress(e) {
 function playSound(element) {
     element.currentTime = 0;
     element.play();
+    recordSound(element);
+}
+function recordSound(audioElement) {
+    tracks.forEach(function (track) {
+        if (track.isRecording) {
+            track.recordSound(audioElement);
+        }
+    });
 }
 function simulateControlClick(key) {
     var control = AudioControls.controls[key];
@@ -111,5 +252,15 @@ function getAudioElement(_a) {
     el.dataset.name = name;
     el.dataset.key = key;
     return el;
+}
+function handlePlayAllButton() {
+    var btn = document.querySelector("#playAll");
+    btn.addEventListener("click", function () {
+        tracks.forEach(function (track) {
+            if (!track.isMuted) {
+                track.playRecording();
+            }
+        });
+    });
 }
 initApp();
--- lab2/src/main.ts
+++ lab2/src/main.ts
@@ -52,16 +52,160 @@ const SOUNDS: Sound[] = [
   },
 ];
 
+interface SoundData {
+  audioElement: HTMLAudioElement;
+  recordTime: number;
+}
+class Track {
+  memory: SoundData[] = [];
+  duration: number;
+  recordingStartTimestamp: number;
+  isRecording: boolean = false;
+  isPlaying: boolean = false;
+  isMuted: boolean = false;
+  currentProgress: number = 0;
+  trackElement: HTMLDivElement;
+
+  constructor(wrapper: HTMLDivElement) {
+    this.create(wrapper);
+  }
+  createTrackControls(): HTMLDivElement {
+    const controlsWrapper: HTMLDivElement = document.createElement("div");
+    controlsWrapper.classList.add("controlsWrapper");
+
+    const muteIcon: HTMLSpanElement = document.createElement("span");
+    muteIcon.classList.add("muteIcon");
+    muteIcon.addEventListener("click", () => {
+      if (!this.isMuted) {
+        muteIcon.classList.add("muteIcon--muted");
+        this.isMuted = true;
+      } else {
+        muteIcon.classList.remove("muteIcon--muted");
+        this.isMuted = false;
+      }
+    });
+
+    const playButton: HTMLButtonElement = document.createElement("button");
+    playButton.classList.add("button", "playButton");
+    playButton.innerText = "ODTWORZ";
+    playButton.addEventListener("click", (e) => {
+      if (this.memory.length > 0) {
+        if (!this.isPlaying) {
+          this.playRecording();
+        }
+      }
+    });
+
+    const recordButton: HTMLButtonElement = document.createElement("button");
+    recordButton.classList.add("button", "recordButton");
+    recordButton.innerText = "NAGRYWAJ";
+    recordButton.addEventListener("click", () => {
+      if (!this.isRecording) {
+        recordButton.classList.add("recordButton--recording");
+        this.startRecording();
+      } else {
+        recordButton.classList.remove("recordButton--recording");
+        this.stopRecording();
+      }
+    });
+
+    controlsWrapper.appendChild(muteIcon);
+    controlsWrapper.appendChild(recordButton);
+    controlsWrapper.appendChild(playButton);
+
+    return controlsWrapper;
+  }
+  create(wrapper: HTMLDivElement) {
+    const trackWrapper: HTMLDivElement = document.createElement("div");
+    trackWrapper.classList.add("trackWrapper");
+    const trackControls: HTMLDivElement = this.createTrackControls();
+    const el: HTMLDivElement = document.createElement("div");
+    el.classList.add("track");
+    trackWrapper.appendChild(trackControls);
+    trackWrapper.appendChild(el);
+    wrapper.appendChild(trackWrapper);
+
+    this.trackElement = el;
+  }
+  clearMemory(): void {
+    this.memory = [];
+    this.duration = 0;
+    this.recordingStartTimestamp = new Date().getTime();
+  }
+  recordSound(audioElement: HTMLAudioElement): void {
+    const recordTimestamp: number = new Date().getTime();
+    const soundData = {
+      audioElement,
+      recordTime: recordTimestamp - this.recordingStartTimestamp,
+    };
+    this.memory.push(soundData);
+  }
+  startRecording(): void {
+    this.clearMemory();
+    this.isRecording = true;
+  }
+  stopRecording(): void {
+    const recordingEndTimestamp = new Date().getTime();
+    this.duration = recordingEndTimestamp - this.recordingStartTimestamp;
+    console.log(this.duration);
+    this.isRecording = false;
+  }
+  playRecording(): void {
+    this.isPlaying = true;
+    this.runTrackProgress();
+    this.memory.forEach((soundData: SoundData, soundIndex, soundsArr) => {
+      setTimeout(() => {
+        playSound(soundData.audioElement);
+      }, soundData.recordTime);
+    });
+  }
+  runTrackProgress(): void {
+    const interval = 1000 / 60;
+    const progressInterval = setInterval(() => {
+      this.currentProgress =
+        this.currentProgress + (interval / this.duration) * 100;
+      if (this.currentProgress >= 100) {
+        this.currentProgress = 100;
+        this.isPlaying = false;
+        this.stopTrackProgress();
+        clearInterval(progressInterval);
+      }
+      this.trackElement.style.background = `linear-gradient(to right, red,  red ${this.currentProgress}%, gray ${this.currentProgress}%, gray 100%)`;
+    }, interval);
+  }
+  stopTrackProgress(): void {
+    setTimeout(() => {
+      this.trackElement.style.background = "";
+      this.currentProgress = 0;
+    }, 500);
+  }
+  removeRecording(): void {
+    this.memory = [];
+  }
+}
+
+const tracks: Track[] = [];
+
 function initApp() {
   createAudioElements();
   createControls();
+  createTracks();
   addKeyListener();
+  handlePlayAllButton();
 }
 
 const AudioControls = {
   elements: {},
   controls: {},
 };
+
+function createTracks(): void {
+  for (let i = 0; i < 4; i++) {
+    const wrapper: HTMLDivElement = document.querySelector("#tracksWrapper");
+    const track: Track = new Track(wrapper);
+    tracks.push(track);
+  }
+}
 
 function createAudioElements(): void {
   const wrapper: HTMLDivElement = document.querySelector("#audioWrapper");
@@ -97,6 +241,15 @@ function handleKeyPress(e: KeyboardEvent): void {
 function playSound(element: HTMLAudioElement): void {
   element.currentTime = 0;
   element.play();
+  recordSound(element);
+}
+
+function recordSound(audioElement: HTMLAudioElement): void {
+  tracks.forEach((track) => {
+    if (track.isRecording) {
+      track.recordSound(audioElement);
+    }
+  });
 }
 
 function simulateControlClick(key: string): void {
@@ -129,6 +282,17 @@ function getAudioElement({ name, src, key }: Sound): HTMLAudioElement {
   el.dataset.name = name;
   el.dataset.key = key;
   return el;
+}
+
+function handlePlayAllButton(): void {
+  const btn: HTMLButtonElement = document.querySelector("#playAll");
+  btn.addEventListener("click", () => {
+    tracks.forEach((track) => {
+      if (!track.isMuted) {
+        track.playRecording();
+      }
+    });
+  });
 }
 
 initApp();
