c8862e0e142b624f5a0d1298276257b8c76b5664
damyanz <damyan.z@o2.pl>
2021-06-13 17:52:28 +0200

    Add Notes unit tests

--- lab4/__tests__/Notes.test.ts
+++ lab4/__tests__/Notes.test.ts
@@ -1,11 +1,21 @@
 import Notes from "../src/Notes";
-import { Provider } from "./storage";
+import { Provider } from "../src/storage";
 
 describe("Notes", () => {
   const notes = new Notes(
-    Provider.localStorage,
+    Provider.LocalStorage,
     "storage_key",
     document.createElement("div"),
     document.createElement("div")
   );
+
+  test("should add note", () => {
+    notes.add("title", "content", "#ff0000", "noteId");
+    expect(Object.keys(notes.notes)).toHaveLength(1);
+  });
+
+  test("should remove note", () => {
+    notes.remove("noteId");
+    expect(Object.keys(notes.notes)).toHaveLength(0);
+  });
 });
